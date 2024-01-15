a61f7cd63043b029bef4bc256066422576ac5018
Tilo Neumann <tilo.neumann@gbv.de>
2024-01-15 15:30:54 +0100

    DMA-240 fix syntax error

--- run.php
+++ run.php
@@ -47,7 +47,7 @@ $settings = [
     'type' => $type,
     'batchSize' => 10000,
     'maxCount' => (isset($argv[2]) == true ? $argv[2] : '10000000000'),
-    'hotfolder' => $destinationPath
+    'hotfolder' => $destinationPath,
     'baseUrl' => 'https://geodatendienste.denkmalatlas.niedersachsen.de/doorman/auth/nld_dda_vektor_wfs'
   ],
   'deleter' => [
