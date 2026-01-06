54898911631225415cf3caa874593fc6703331cc
Tilo Neumann <tilo.neumann@gbv.de>
2026-01-06 11:22:17 +0100

    adds import script for cron

---
+++ importLastChanges.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# make sure skript ends on error
+set -euo pipefail
+
+# yesterday
+DATE=$(date -d "yesterday" +%F)
+
+# get latest changes
+/usr/bin/php /opt/digiverso/kult_dma_object_importer/run.php --limit=100 -from="$DATE"
+
+# move media files
+find /opt/digiverso/viewer/coldfolder/ -maxdepth 1 -type d -name '*_media' \
+  -exec mv -t /opt/digiverso/viewer/hotfolder/ {} +
+
+# make tomcat owner to prevent errors on deleting
+chown -R tomcat:tomcat /opt/digiverso/viewer/hotfolder/
+
+# start import
+find /opt/digiverso/viewer/coldfolder/ -maxdepth 1 -type f -name '*.xml' \
+  -exec mv -t /opt/digiverso/viewer/hotfolder/ {} +
+
+# update locate db
+updatedb
