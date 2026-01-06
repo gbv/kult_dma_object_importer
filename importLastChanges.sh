14e677bf1fe16e194ad6ec8b00d7fe2131365bc0
Tilo Neumann <tilo.neumann@gbv.de>
2026-01-06 12:09:42 +0100

    adds time to log

--- importLastChanges.sh
+++ importLastChanges.sh
@@ -9,24 +9,24 @@ LOGDATE=$(date +%F)
 LOGFILE="$LOGDIR/import-$LOGDATE.log"
 exec >>"$LOGFILE" 2>&1
 
-echo "Logfile created."
+echo "[$(date)] Logfile created."
 
 updatedb
-echo "Locate updated."
+echo "[$(date)] Locate updated."
 
 DATE=$(date -d "yesterday" +%F)
-echo "Set Date to yesterday: $DATE"
+echo "[$(date)] Set Date to yesterday: $DATE"
 
 /usr/bin/php /opt/digiverso/kult_dma_object_importer/run.php --limit=100 --from="$DATE"
-echo "Got latest changes."
+echo "[$(date)] Got latest changes."
 
 find /opt/digiverso/viewer/coldfolder/ -maxdepth 1 -type d -name '*_media' \
   -exec mv -t /opt/digiverso/viewer/hotfolder/ {} +
-echo "Media files moved."
+echo "[$(date)] Media files moved."
 
 chown -R tomcat:tomcat /opt/digiverso/viewer/hotfolder/
-echo "Tomcat own media files now."
+echo "[$(date)] Tomcat own media files now."
 
 find /opt/digiverso/viewer/coldfolder/ -maxdepth 1 -type f -name '*.xml' \
   -exec mv -t /opt/digiverso/viewer/hotfolder/ {} +
-echo "Hotfolder filled with import documents."
+echo "[$(date)] Hotfolder filled with import documents."
